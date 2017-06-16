91643f645785d6fdbcaaef1ad70d644105807498
Thomas van Dam <t.vandam@wearetriple.com>
2017-06-16 18:26:48 +0200

    feat(time): Add duration and currentTime property accessors
    This allows the player to get correct time information without waiting
    for events to trigger. This required renaming the old duration property
    to mediaDuration. Also move the seek and timeUpdate events to the media
    plugin prototype. Part of meisterplayer/meisterplayer#36.

--- src/js/Hls.js
+++ src/js/Hls.js
@@ -78,7 +78,7 @@ class Hls extends Meister.MediaPlugin {
 
         this.previousLevel = -1;
         this.lowestLevel = 0;
-        this.duration = 0;
+        this.mediaDuration = 0;
         this.item = null;
     }
 
@@ -111,13 +111,8 @@ class Hls extends Meister.MediaPlugin {
                 autoStartLoad: false,
             });
 
-            // Display the correct title.
-            this.on('_playerTimeUpdate', this._onPlayerTimeUpdate.bind(this));
-            this.on('_playerSeek', this._onPlayerSeek.bind(this));
-            this.on('requestSeek', this.onRequestSeek.bind(this));
-            this.on('playerError', this.onPlayerError.bind(this));
-
             // Listen to control events.
+            this.on('playerError', this.onPlayerError.bind(this));
             this.on('requestBitrate', this.onRequestBitrate.bind(this));
             this.on('requestGoLive', () => this.onRequestGoLive());
 
@@ -193,9 +188,9 @@ class Hls extends Meister.MediaPlugin {
                 const live = data.details.live;
                 let hasDVR = false;
 
-                this.duration = data.details.totalduration;
+                this.mediaDuration = data.details.totalduration;
 
-                if (live && this.duration > this.dvrThreshold) {
+                if (live && this.mediaDuration > this.dvrThreshold) {
                     hasDVR = true;
                 }
 
@@ -206,7 +201,7 @@ class Hls extends Meister.MediaPlugin {
                 this.meister.trigger('itemTimeInfo', {
                     hasDVR,
                     isLive: live,
-                    duration: this.duration,
+                    duration: this.mediaDuration,
                 });
             });
 
@@ -253,6 +248,26 @@ class Hls extends Meister.MediaPlugin {
         });
     }
 
+    get duration() {
+        if (!this.hls) { return NaN; }
+
+        return this.mediaDuration;
+    }
+
+    get currentTime() {
+        if (!this.hls || !this.player) { return NaN; }
+
+        const playOffset = this.player.duration - this.mediaDuration;
+        return this.player.currentTime - playOffset;
+    }
+
+    set currentTime(time) {
+        if (!this.hls || !this.player) { return; }
+
+        const playOffset = this.player.duration - this.mediaDuration;
+        this.player.currentTime = time + playOffset;
+    }
+
     playerLoadedMetadata() {
         // override the base function because HLSjs has its own startPosition handling
     }
@@ -269,21 +284,21 @@ class Hls extends Meister.MediaPlugin {
     }
 
     _onPlayerTimeUpdate() {
-        const playOffset = this.meister.duration - this.duration;
+        const playOffset = this.player.duration - this.mediaDuration;
 
         this.meister.trigger('playerTimeUpdate', {
-            currentTime: this.meister.currentTime - playOffset,
-            duration: this.duration,
+            currentTime: this.player.currentTime - playOffset,
+            duration: this.mediaDuration,
         });
 
         this.broadcastTitle();
     }
 
     _onPlayerSeek() {
-        const playOffset = this.meister.duration - this.duration;
+        const playOffset = this.player.duration - this.mediaDuration;
 
-        const currentTime = this.meister.currentTime - playOffset;
-        const duration = this.duration;
+        const currentTime = this.player.currentTime - playOffset;
+        const duration = this.mediaDuration;
         const relativePosition = currentTime / duration;
 
         this.meister.trigger('playerSeek', {
@@ -297,24 +312,24 @@ class Hls extends Meister.MediaPlugin {
         let targetTime;
 
         if (Number.isFinite(e.relativePosition)) {
-            const playOffset = this.meister.duration - this.duration;
-            targetTime = (this.duration * e.relativePosition) + playOffset;
+            const playOffset = this.player.duration - this.mediaDuration;
+            targetTime = (this.mediaDuration * e.relativePosition) + playOffset;
         } else if (Number.isFinite(e.timeOffset)) {
-            targetTime = this.meister.currentTime + e.timeOffset;
+            targetTime = this.player.currentTime + e.timeOffset;
         } else if (Number.isFinite(e.targetTime)) {
-            const playOffset = this.meister.duration - this.duration;
+            const playOffset = this.player.duration - this.mediaDuration;
             targetTime = e.targetTime + playOffset;
         }
 
-        if (!e.forcedStart && this.blockSeekForward && targetTime > this.meister.currentTime) { return; }
+        if (!e.forcedStart && this.blockSeekForward && targetTime > this.player.currentTime) { return; }
 
         if (Number.isFinite(targetTime)) {
-            this.meister.currentTime = targetTime;
+            this.player.currentTime = targetTime;
         }
     }
 
     onRequestGoLive() {
-        const duration = this.meister.duration;
+        const duration = this.player.duration;
         const targetDuration = this.hls.levels[this.hls.currentLevel].details.targetduration;
         const liveSync = this.hls.config.liveSyncDurationCount;
 
@@ -322,7 +337,7 @@ class Hls extends Meister.MediaPlugin {
 
         const liveTime = duration - liveOffset;
 
-        this.meister.currentTime = liveTime;
+        this.player.currentTime = liveTime;
     }
 
     processMetadata(data) {
@@ -346,7 +361,7 @@ class Hls extends Meister.MediaPlugin {
     }
 
     broadcastTitle() {
-        const time = this.meister.currentTime;
+        const time = this.player.currentTime;
         // No need to spam events.
         if (this.previousMetadata &&
                 (this.previousMetadata.start < time && time < this.previousMetadata.end)
@@ -525,7 +540,7 @@ class Hls extends Meister.MediaPlugin {
 
         // Traverse backwards since it is more likely that the player is near the end
         let data = null;
-        const time = this.meister.currentTime;
+        const time = this.player.currentTime;
         for (let i = this.metadata.length - 1; i >= 0; i--) {
             if (this.metadata[i].start < time && time < this.metadata[i].end) {
                 data = this.metadata[i];
